d1e0c3e54e16e9196e3fd4d71632a205256df150
Nico Bauer <Bauer.Nico1998@outlook.de>
2020-08-18 12:59:27 +0200

    Änderung

--- Beliebige Zahl/Program.cs
+++ Beliebige Zahl/Program.cs
@@ -13,7 +13,40 @@ namespace Beliebige_Zahl
     {
         static void Main(string[] args)
         {
-            
+            int zahl;
+            bool groeßer0;
+            bool gleich0;
+            bool gerade;
+
+            groeßer0 = zahl > 0 ? false : zahl < 0 ? false : true;
+            gleich0 = zahl > 0 ? true : false;
+
+            if (!gleich0)
+            {
+                gerade = zahl % 2 == 0 ? true : false;
+
+                Console.WriteLine("Der angegebene Wert ist groesser als Null.\n");
+                if (gerade)
+                {
+                    Console.WriteLine("Der angegebene Wert ist gerade.\n");
+                }
+                else
+                {
+                    Console.WriteLine("Der angegebene Wert ist ungerade.\n")
+                }
+            }
+            else
+            {
+                Console.WriteLine("Der angegebene Wert ist kleiner als Null.");
+                if (gerade)
+                {
+                    Console.WriteLine("Der angegebene Wert ist gerade.\n");
+                }
+                else
+                {
+                    Console.WriteLine("Der angegebene Wert ist ungerade.\n");
+                }
+            }
         }
     }
 }
