83dea585065f9537cf6ea3a32702bafe9db3f066
Nico Bauer <Bauer.Nico1998@outlook.de>
2020-08-18 12:41:55 +0200

    Änderung

---
+++ Beliebige Zahl/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+/// <summary>
+/// Nico Bauer
+/// IS218
+/// AWE
+/// Eine beliebige Zahl wird eingelesen. Bestimme, ob die Zahl größer, gleich oder kleiner Null ist. Gib weiterhin an, ob die Zahl gerade oder ungerade ist! (Hinweis: modulo-Operator %)
+/// </summary>
+
+namespace Beliebige_Zahl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
